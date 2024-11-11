0a77012611de1207f95a2b7294dd94e89293bc24
Lassi Munter <lassi.munter@gmail.com>
2024-11-11 15:56:57 +0200

    pelaajalista refaktoroitu

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,16 +1,11 @@
 import requests
-from player import Player
+from player import Player, PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        if player.nationality == "FIN":
-            players.append(player)
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     print("Players from FIN:")
 
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,5 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
@@ -5,6 +7,30 @@ class Player:
         self.goals = dict['goals']
         self.assists = dict['assists']
         self.team = dict['team']
+        self.points = self.goals + self.assists
     
     def __str__(self):
-        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
+        return f"{self.name:20} {self.team:5} {self.goals} + {self.assists} = {self.points}"
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        filtered_players = []
+        for player in self.reader:
+            if player.nationality == nationality:
+                filtered_players.append(player)
+        sorted_players = sorted(filtered_players, key=lambda x: x.points, reverse=True)
+        return sorted_players
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        player_dict = requests.get(self.url).json()
+        player_list = []
+        for player in player_dict:
+            player_list.append(Player(player))
+        return player_list
