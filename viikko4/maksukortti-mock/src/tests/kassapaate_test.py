7d80a21f4cf4a196e54fdf050becb465d93cbe87
Lassi Munter <lassi.munter@gmail.com>
2024-11-21 20:10:58 +0200

    viikko 4 tehtävä 2 tehty

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,15 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_voi_ladata_rahaa(self):
+        maksukortti_mock = Mock()
+        self.kassa.lataa(maksukortti_mock, 10)
+
+        maksukortti_mock.lataa.assert_called_with(10)
+
+    def test_kortille_ei_voi_lataa_negatiivista_summaa(self):
+        maksukortti_mock = Mock()
+        self.kassa.lataa(maksukortti_mock, -10)
+
+        maksukortti_mock.lataa.assert_not_called()
